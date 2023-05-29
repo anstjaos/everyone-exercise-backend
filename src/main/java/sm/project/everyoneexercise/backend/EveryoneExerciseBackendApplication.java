13b4e3dbd4c970f82ffbfc627a3702efad2e6eaf
anstjaos <moonseo.kim@nhn.com>
2023-05-29 14:16:17 +0900

    패키지명 변경

--- src/main/java/sm/project/everyoneexercise/backend/EveryoneExerciseBackendApplication.java
+++ src/main/java/sm/project/everyoneexercise/backend/EveryoneExerciseBackendApplication.java
@@ -1,4 +1,4 @@
-package sm.project.everyoneexercisebackend;
+package sm.project.everyoneexercise.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/sm/project/everyoneexercise/backend/EveryoneExerciseBackendApplicationTests.java
+++ src/test/java/sm/project/everyoneexercise/backend/EveryoneExerciseBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package sm.project.everyoneexercisebackend;
+package sm.project.everyoneexercise.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
