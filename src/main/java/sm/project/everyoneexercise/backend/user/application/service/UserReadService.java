5e4413b37ad05f6e138268bf386f54c5edc7e957
anstjaos <vv_eros@naver.com>
2023-06-25 15:59:27 +0900

    :sparkles: 회원 조회 API 개발

--- src/main/java/sm/project/everyoneexercise/backend/user/application/service/UserReadService.java
+++ src/main/java/sm/project/everyoneexercise/backend/user/application/service/UserReadService.java
@@ -1,13 +1,13 @@
 package sm.project.everyoneexercise.backend.user.application.service;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import sm.project.everyoneexercise.backend.user.application.port.in.ReadUserUseCase;
 import sm.project.everyoneexercise.backend.user.application.port.out.ReadUserPort;
 import sm.project.everyoneexercise.backend.user.domain.User;
 
 @AllArgsConstructor
-@Slf4j
+@Service
 class UserReadService implements ReadUserUseCase {
     private final ReadUserPort readUserPort;
 
