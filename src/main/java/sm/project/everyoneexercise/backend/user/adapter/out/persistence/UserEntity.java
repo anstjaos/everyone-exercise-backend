0e0fa5cd60670188c203774ee233949b7bcdf988
anstjaos <vv_eros@naver.com>
2023-08-24 21:52:22 +0900

    :art: 불필요한 테스트 코드 제거 및 TODO 해결

--- src/main/java/sm/project/everyoneexercise/backend/user/adapter/out/persistence/UserEntity.java
+++ src/main/java/sm/project/everyoneexercise/backend/user/adapter/out/persistence/UserEntity.java
@@ -2,11 +2,10 @@ package sm.project.everyoneexercise.backend.user.adapter.out.persistence;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import sm.project.everyoneexercise.backend.user.application.port.in.UpdateUserCommand;
-
-import java.util.Optional;
 
 @Document(collection = "user")
 @Getter
@@ -31,17 +30,20 @@ public class UserEntity {
     }
 
     public void updateUser(UpdateUserCommand updateUserCommand) {
-        // TODO : if 문으로 수정
-        Optional.ofNullable(updateUserCommand.nickname())
-                .ifPresent(nickname -> this.nickname = nickname);
+        if (StringUtils.isNotBlank(updateUserCommand.nickname())) {
+            this.nickname = updateUserCommand.nickname();
+        }
 
-        Optional.ofNullable(updateUserCommand.password())
-                .ifPresent(password -> this.password = password);
+        if (StringUtils.isNotBlank(updateUserCommand.password())) {
+            this.password = updateUserCommand.password();
+        }
 
-        Optional.ofNullable(updateUserCommand.phoneNumber())
-                .ifPresent(phoneNumber -> this.phoneNumber = phoneNumber);
+        if (StringUtils.isNotBlank(updateUserCommand.phoneNumber())) {
+            this.phoneNumber = updateUserCommand.phoneNumber();
+        }
 
-        Optional.ofNullable(updateUserCommand.autoLogin())
-                .ifPresent(autoLogin -> this.autoLogin = autoLogin);
+        if (updateUserCommand.autoLogin() != null) {
+            this.autoLogin = updateUserCommand.autoLogin();
+        }
     }
 }
